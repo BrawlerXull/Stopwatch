56ae5b36737de35af19855c4f055a2d8e70ca316
Chinmay <chaudhari.chinmay12345@gmail.com>
2023-07-09 22:10:22 +0530

    number picker added

--- lib/screens/pomodoropage/view/PomodoroPage.dart
+++ lib/screens/pomodoropage/view/PomodoroPage.dart
@@ -1,5 +1,7 @@
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_time_picker_spinner/flutter_time_picker_spinner.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class PomodoroPage extends StatefulWidget {
   const PomodoroPage({super.key});
@@ -9,16 +11,22 @@ class PomodoroPage extends StatefulWidget {
 }
 
 class _PomodoroPageState extends State<PomodoroPage> {
+  final CountDownController _controller = CountDownController();
+  int _valueTimer = 10;
+  int _valueBreak = 10;
+  // DateTime? _time;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF0E1116),
+      backgroundColor: const Color(0xFF0E1116),
       body: SafeArea(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              SizedBox(
+                height: 40,
+              ),
               const Text(
                 "Pomodoro",
                 style: TextStyle(
@@ -26,20 +34,80 @@ class _PomodoroPageState extends State<PomodoroPage> {
                     fontSize: 40,
                     fontWeight: FontWeight.bold),
               ),
+              SizedBox(
+                height: 80,
+              ),
               CircularCountDownTimer(
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: MediaQuery.of(context).size.width / 2,
-                  duration: 10,
-                  isTimerTextShown: true,
-                  isReverseAnimation: false,
-                  isReverse: true,
-                  textStyle: const TextStyle(
-                      fontSize: 70.0,
+                controller: _controller,
+                width: MediaQuery.of(context).size.width / 2,
+                height: MediaQuery.of(context).size.width / 2,
+                duration: 10,
+                isTimerTextShown: true,
+                isReverseAnimation: false,
+                isReverse: true,
+                textStyle: const TextStyle(
+                    fontSize: 70.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+                textFormat: CountdownTextFormat.S,
+                fillColor: Colors.red,
+                ringColor: Colors.blue,
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Timer Minutes",
+                    style: TextStyle(
                       color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                  textFormat: CountdownTextFormat.S,
-                  fillColor: Colors.red,
-                  ringColor: Colors.blue),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  NumberPicker(
+                    minValue: 1,
+                    maxValue: 60,
+                    value: _valueTimer,
+                    onChanged: (value) {
+                      setState(() {
+                        _valueTimer = value;
+                      });
+                    },
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Break Timer",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  NumberPicker(
+                    minValue: 1,
+                    maxValue: 60,
+                    value: _valueBreak,
+                    onChanged: (value) {
+                      setState(() {
+                        _valueBreak = value;
+                      });
+                    },
+                  ),
+                ],
+              ),
             ],
           ),
         ),
